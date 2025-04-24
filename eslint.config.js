da7d62059078cdbf050a66cda1487f29c835ba70
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-04-24 11:21:34 +0200

    chore : add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      semi: "error",
+      indent: ["error", 2],
+    },
+  },
+  // MODIF ICI
+  {
+    // Configuration spécifique pour HTML et CSS
+    files: ["/*.html", "/*.css"],
+    rules: {
+      indent: ["error", 4],
+    },
+  },
+];
