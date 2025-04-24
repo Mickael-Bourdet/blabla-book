d4608f10de9370c480c6a3a10a9a4cc84c3753a8
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-04-24 10:56:11 +0200

    fix: eslint config

--- back/src/models/Book.ts
+++ back/src/models/Book.ts
@@ -3,56 +3,56 @@ import { sequelize } from './client-sequelize';
 
 
 export class Book extends Model<InferAttributes<Book>, InferCreationAttributes<Book>> {
-        declare id: CreationOptional<number>;
-        declare isbn: string;
-        declare title: string;
-        declare description: string | null;
-        declare published: string | null;
-        declare cover_url: string | null;
-        declare page_count: string;
-    }
+  declare id: CreationOptional<number>;
+  declare isbn: string;
+  declare title: string;
+  declare description: string | null;
+  declare published: string | null;
+  declare cover_url: string | null;
+  declare page_count: string;
+}
 
 Book.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
 
-        isbn: {
-            type: DataTypes.STRING(13),
-            allowNull: false,
-            unique: true,
-        },
+    isbn: {
+      type: DataTypes.STRING(13),
+      allowNull: false,
+      unique: true,
+    },
 
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
 
-        published: {
-            type: DataTypes.DATE,
-            allowNull: true,
-        },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
 
-        cover_url: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
+    cover_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
 
-        page_count: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+    page_count: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-    }, {
-        sequelize,
-        tableName: 'book',
-    }
+  }, {
+  sequelize,
+  tableName: 'book',
+}
 )
--- back/src/models/associations.ts
+++ back/src/models/associations.ts
@@ -1,49 +1,49 @@
-import { sequelize } from './client-sequelize.js';
-import { Author } from './Author.js';
-import { Book } from './Book.js';
-import { Category } from './Category.js';
-import { User } from './User.js';
+import { sequelize } from './client-sequelize';
+import { Author } from './Author';
+import { Book } from './Book';
+import { Category } from './Category';
+import { User } from './User';
 
 Book.belongsToMany(Author, {
-    through: "book_author",
-    as: "authors",
-    foreignKey: "book_id",
-    otherKey: "author_id"
+  through: "book_author",
+  as: "authors",
+  foreignKey: "book_id",
+  otherKey: "author_id"
 });
 
 Author.belongsToMany(Book, {
-    through: "book_author",
-    as: "books",
-    foreignKey: "auhtor_id",
-    otherKey: "book_id"
+  through: "book_author",
+  as: "books",
+  foreignKey: "auhtor_id",
+  otherKey: "book_id"
 });
 
 Book.belongsToMany(Category, {
-    through: "book_category",
-    as: "categories",
-    foreignKey: "book_id",
-    otherKey: "category_id"
+  through: "book_category",
+  as: "categories",
+  foreignKey: "book_id",
+  otherKey: "category_id"
 });
 
 Category.belongsToMany(Book, {
-    through: "book_category",
-    as: "books",
-    foreignkey: "categroy_id",
-    otherkey: "book_id"
+  through: "book_category",
+  as: "books",
+  foreignkey: "categroy_id",
+  otherkey: "book_id"
 });
 
 Book.belongsToMany(User, {
-    through: "book_user",
-    as: "users",
-    foreignKey: "book_id",
-    otherKey: "user_id"
+  through: "book_user",
+  as: "users",
+  foreignKey: "book_id",
+  otherKey: "user_id"
 });
 
 User.belongsToMany(Book, {
-    through: "book_user",
-    as: "books",
-    foreignkey: "user_id",
-    otherkey: "book_id"
+  through: "book_user",
+  as: "books",
+  foreignkey: "user_id",
+  otherkey: "book_id"
 });
 
 export { Author, Book, Category, User, sequelize };
