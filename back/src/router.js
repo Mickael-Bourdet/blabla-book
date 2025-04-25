161af545e1e0d59a4aaca63b55e78e41fc45d49e
Axel-Chat <you@example.com>
2025-04-25 16:33:25 +0200

    back:userLibraryController comment

--- back/src/router.js
+++ back/src/router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { bookController } from "./controller/bookController.js";
 import { userController } from "./controller/userController.js";
+import {userLibraryController} from "./controller/userLibraryController.js"
 import { validate } from "./middlewares/validateWrapper.js";
-import { schema } from "./middlewares/userValidateSchema.js";
+import { schema } from "./middlewares/schemaValidate/userValidateSchema.js";
 
 
 const router = Router();
@@ -15,5 +16,17 @@ router.get("/books/:bookId", bookController.getOneBook);
 router.get("/user/:userId", userController.getOneUser);
 router.patch("/user/:userId",validate(schema), userController.updateUser);
 router.delete("/user/:userId", validate(schema),userController.deleteUser);
+
+//userLibrary Routes
+router.get("/user/library/:userId", userLibraryController.getLibrary);
+
+//Read
+router.post("/user/:userId/books/read/:bookId",validate(schema), userLibraryController.addRead);
+router.delete("/user/:userId/books/read/:bookId",validate(schema), userLibraryController.deleteRead);
+
+//Toread
+router.post("/user/:userId/books/to-read/:bookId",validate(schema), userLibraryController.addToRead);
+router.delete("/user/:userId/books/to-read/:bookId",validate(schema), userLibraryController.deleteToRead);
+
 
 export { router };
