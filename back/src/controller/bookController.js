0a827e1c900b845923d1fa48e8a6432ea68f52fd
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-04-25 06:12:07 +0200

    feat: getOneBook route and controller

--- back/src/controller/bookController.js
+++ back/src/controller/bookController.js
@@ -4,6 +4,19 @@ const bookController = {
   async getAllBooks(req, res) {
     res.status(200).json(await Book.findAll({}));
   },
+
+  async getOneBook(req, res, next) {
+    const id = req.params.bookId;
+  
+    const result = await Book.findByPk(id);
+
+    // checking if result exist, if it's not, go to the middleware errorHandler
+    // if (!result) {
+    //   return next();
+    // }
+
+    res.status(200).json(result);
+  }
 };
 
 export { bookController };
--- back/src/router.js
+++ back/src/router.js
@@ -4,5 +4,6 @@ import { bookController } from "./controller/bookController.js";
 const router = Router();
 
 router.get("/books", bookController.getAllBooks);
+router.get("/books/:bookId", bookController.getOneBook);
 
 export { router };
