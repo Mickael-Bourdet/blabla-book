4124c90fee54b7a368ba657be606159f35e7d9c9
Axel-Chat <you@example.com>
2025-04-26 09:24:06 +0200

    fix:  component popularbooks and recommended

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,7 +12,6 @@ function App() {
      <Navbars/>
       <Routes>
      <Route path='/' element={<HomePage/>} />
-    
      </Routes>
      <Footer/>
     </>
--- front/src/components/PopularsBooks.tsx
+++ front/src/components/PopularsBooks.tsx
@@ -14,49 +14,38 @@ const PopularBooks = () => {
   
                 <h2 className="text-xl mb-4 font-bold mt-[100px] ">Livres populaires</h2>
                 <div className="book-list grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
-                <a href="/books/bookid" className="block">
-                  <div className="book text-center cursor-pointer hover:shadow-lg hover:rounded-md transition-shadow">
-                      
-                    <img 
-                      src="https://m.media-amazon.com/images/I/81drfTT9ZfL.jpg" 
-                      alt="Harry Potter à l'école des sorciers" 
-                      className="h-auto w-full object-cover mb-2" 
-                    />
-                    <p>Harry Potter à l'école des sorciers</p>
-                  </div>
-                  </a>
-                  <div className="book text-center">
-                    <img 
-                      src="https://m.media-amazon.com/images/I/81drfTT9ZfL.jpg" 
-                      alt="Harry Potter à l'école des sorciers" 
-                      className="h-95 w-auto object-cover mb-2"
-                    />
-                    <p>Harry Potter à l'école des sorciers</p>
-                  </div>
-                  <div className="book text-center">
-                    <img 
-                      src="https://m.media-amazon.com/images/I/81drfTT9ZfL.jpg" 
-                      alt="Harry Potter à l'école des sorciers" 
-                      className="h-95 w-auto object-cover mb-2"
-                    />
-                    <p>Harry Potter à l'école des sorciers</p>
-                  </div>
-                  <div className="book text-center">
-                    <img 
-                      src="https://m.media-amazon.com/images/I/81drfTT9ZfL.jpg" 
-                      alt="Harry Potter à l'école des sorciers" 
-                      className="h-95 w-auto object-cover mb-2"
-                    />
-                    <p>Harry Potter à l'école des sorciers</p>
-                  </div>
-                  <div className="book text-center">
-                    <img 
-                      src="https://m.media-amazon.com/images/I/81drfTT9ZfL.jpg" 
-                      alt="Harry Potter à l'école des sorciers" 
-                      className="h-95 w-auto object-cover mb-2"
-                    />
-                    <p>Harry Potter à l'école des sorciers</p>
-                  </div>
+               
+
+            <a href="/books/bookid" className="block">
+                <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+                     <img src="https://m.media-amazon.com/images/I/91b0C2YNSrL.jpg" alt="Le Comte de Monte-Cristo - Dumas" className="h-auto w-full object-cover mb-2" />
+                     <p>Le Comte de Monte-Cristo</p>
+                </div>
+            </a>
+            <a href="/books/bookid" className="block">
+                <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+                     <img src="https://m.media-amazon.com/images/I/91b0C2YNSrL.jpg" alt="Le Comte de Monte-Cristo - Dumas" className="h-auto w-full object-cover mb-2" />
+                     <p>Le Comte de Monte-Cristo</p>
+                </div>
+            </a>
+            <a href="/books/bookid" className="block">
+                <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+                     <img src="https://m.media-amazon.com/images/I/91b0C2YNSrL.jpg" alt="Le Comte de Monte-Cristo - Dumas" className="h-auto w-full object-cover mb-2" />
+                     <p>Le Comte de Monte-Cristo</p>
+                </div>
+            </a>
+            <a href="/books/bookid" className="block">
+                <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+                     <img src="https://m.media-amazon.com/images/I/91b0C2YNSrL.jpg" alt="Le Comte de Monte-Cristo - Dumas" className="h-auto w-full object-cover mb-2" />
+                     <p>Le Comte de Monte-Cristo</p>
+                </div>
+            </a>
+            <a href="/books/bookid" className="block">
+                <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+                     <img src="https://m.media-amazon.com/images/I/91b0C2YNSrL.jpg" alt="Le Comte de Monte-Cristo - Dumas" className="h-auto w-full object-cover mb-2" />
+                     <p>Le Comte de Monte-Cristo</p>
+                </div>
+            </a>
                 </div>
                 
               </section>
