15ee77da885230c16b69cde82713d085da02670c
AmndineBlt <amandine.bluteau@oclock.school>
2025-04-28 17:44:25 +0200

    refacto: delete files

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,28 +1,19 @@
-<<<<<<< HEAD
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-=======
-import {Route,Routes } from 'react-router-dom';
-import './App.css'
-import HomePage from './pages/Home'
-import Navbars from './components/Navbar';
-import Footer from './components/footer';
-import DetailPage from './pages/DetailsBookPage';
-
->>>>>>> 5e97f4021110b6acc9c77634028cafe4e8e022a5
+import HomePage from "./pages/Home";
+import Navbars from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import DetailPage from "./pages/DetailsBookPage";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <h1 className="bg-red-200">HELLO SUSHI BLABLABOOK</h1>
-=======
-     <Navbars/>
+      <Navbars />
       <Routes>
-     <Route path='/' element={<HomePage/>} />
-     <Route path="/books/bookid" element={<DetailPage/>} />
-     </Routes>
-     <Footer/>
->>>>>>> 5e97f4021110b6acc9c77634028cafe4e8e022a5
+        <Route path="/" element={<HomePage />} />
+        <Route path="/books/bookid" element={<DetailPage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
