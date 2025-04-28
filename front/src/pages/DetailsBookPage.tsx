ef0ba3db77ae949c4c5db9e466d1c94d606f45b1
AmndineBlt <amandine.bluteau@oclock.school>
2025-04-28 11:02:29 +0200

    refactor: space deletion

--- front/src/pages/DetailsBookPage.tsx
+++ front/src/pages/DetailsBookPage.tsx
@@ -1,13 +1,11 @@
-
-
 import BookDetail from "../components/DetailedBook";
 import RecommendedBooks from "../components/RecommendedBooks";
 
 const DetailPage = () => (
-    <>
-      <BookDetail />
-      <RecommendedBooks />
-    </>
-  );
-  
-  export default DetailPage;
+  <>
+    <BookDetail />
+    <RecommendedBooks />
+  </>
+);
+
+export default DetailPage;
