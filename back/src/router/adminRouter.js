c8c3c23a3641a2e6b8768858f28d8562fec716d5
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-04-28 10:58:05 +0200

    docs : comment admin/book routes

--- back/src/router/adminRouter.js
+++ back/src/router/adminRouter.js
@@ -5,14 +5,28 @@ import { createBookSchema, updateBookSchema } from "../middlewares/schemaValidat
 export const router = Router();
 
 /**
- * POST /admin/add/books
+ * POST/admin/add/books
  * @summary add a new book
- * @param {AddNewBookDTO} request.body.required - the book body
  * @return {Book} 201 - the added book
  * @return {Error} 400 - Bad request response (missing property)
  * @return {Error} 409 - Conflict response (ex: isbn already taken)
  */
-
 router.post("/admin/add/books", validate(createBookSchema), adminController.AddNewBook);
+
+/**
+ * PATCH /admin/update/books/:bookId
+ * @summary Edit a book
+ * @return {Book} 200 - success response
+ * @return {Error} 400 - Bad request response
+ * @return {Error} 404 - Book not found
+ * @return {Error} 409 - Conflict response (ex: isbn already taken)
+ */
 router.patch("/admin/update/books/:bookId", validate(updateBookSchema), adminController.updateBook);
+
+/**
+ * DELETE /admin/delete/books/:bookId
+ * @summary Edit a book
+ * @return {Book} 204 - success response no content send
+ * @return {Error} 404 - Book not found
+ */
 router.delete("/admin/delete/books/:bookId", adminController.deleteBook);
