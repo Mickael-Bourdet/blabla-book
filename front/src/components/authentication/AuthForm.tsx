35ed10da298eed6957494dc1250a84015ac490be
AmndineBlt <amandine.bluteau@oclock.school>
2025-04-28 11:23:28 +0200

    add: AuthForm

---
+++ front/src/components/authentication/AuthForm.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type AuthFormProps = {
+  title: string;
+  children: ReactNode;
+};
+
+export default function AuthFormWrapper({ title, children }: AuthFormProps) {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-6 text-center">{title}</h2>
+        {children}
+      </form>
+    </div>
+  );
+}
