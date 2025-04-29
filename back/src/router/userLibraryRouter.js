9328f7274fdfbf541c34c0b5042b4f97f4c2be2e
Axel-Chat <you@example.com>
2025-04-29 11:36:19 +0200

    comment:userLibraryRouter

--- back/src/router/userLibraryRouter.js
+++ back/src/router/userLibraryRouter.js
@@ -4,18 +4,52 @@ import { userLibraryController } from "../controller/userLibraryController.js";
 
 export const router = Router();
 
-// TODO : add comments
+
 /**
  * GET /books
  * @summary Return all books of one user
- * @return {User[]} 200 - success response
+ * @param {number} userId.path.required - ID of the user
+ * @return {User} 200 - success response
  */
 router.get("/user/library/:userId", userLibraryController.getLibrary);
 
+
 //Read
+/**
+ * POST /user/:userId/books/read/:bookId
+ * @summary Add a book to the "already read" list of a user
+ * @param {number} userId.path.required - ID of the user
+ * @param {number} bookId.path.required - ID of the book
+ * @return {object} 200 - success message
+ */
 router.post("/user/:userId/books/read/:bookId", userLibraryController.addToMyReadLibrary);
+
+/**
+ * DELETE /user/:userId/books/read/:bookId
+ * @summary Remove a book from the "already read" list of a user
+ * @param {number} userId.path.required - ID of the user
+ * @param {number} bookId.path.required - ID of the book
+ * @return {object} 200 - success message
+ */
 router.delete("/user/:userId/books/read/:bookId",userLibraryController.deleteToMyReadLibrary);
 
+
 //Toread
+
+/**
+ * POST /user/:userId/books/to-read/:bookId
+ * @summary Add a book to the "to read" list of a user
+ * @param {number} userId.path.required - ID of the user
+ * @param {number} bookId.path.required - ID of the book
+ * @return {object} 200 - success message
+ */
 router.post("/user/:userId/books/to-read/:bookId", userLibraryController.addToWishRead);
+
+/**
+ * DELETE /user/:userId/books/to-read/:bookId
+ * @summary Remove a book from the "to read" list of a user
+ * @param {number} userId.path.required - ID of the user
+ * @param {number} bookId.path.required - ID of the book
+ * @return {object} 200 - success message
+ */
 router.delete("/user/:userId/books/to-read/:bookId",userLibraryController.deleteToWishRead);
