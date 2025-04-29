7922ca16071a89d55f100b14be1d9aae86ac18c1
Axel-Chat <you@example.com>
2025-04-29 13:01:53 +0200

    comment:usercontroller

--- back/src/router/userLibraryRouter.js
+++ back/src/router/userLibraryRouter.js
@@ -4,52 +4,54 @@ import { userLibraryController } from "../controller/userLibraryController.js";
 
 export const router = Router();
 
-
 /**
- * GET /books
- * @summary Return all books of one user
- * @param {number} userId.path.required - ID of the user
- * @return {User} 200 - success response
- */
+* GET /user/library/:userId
+* @summary Return the library of a specific user, including their books.
+* @param {number} userId - The ID of the user whose library is being fetched.
+* @return {User} 200 - Success response with the user's library data.
+* @throws {Error} User not found (404) - If the user is not found.
+*/
 router.get("/user/library/:userId", userLibraryController.getLibrary);
 
 
-//Read
+// Read
+
 /**
  * POST /user/:userId/books/read/:bookId
  * @summary Add a book to the "already read" list of a user
- * @param {number} userId.path.required - ID of the user
- * @param {number} bookId.path.required - ID of the book
- * @return {object} 200 - success message
+ * @param {number} userId.path.required - The ID of the user
+ * @param {number} bookId.path.required - The ID of the book
+ * @return {object} 200 - Success message
  */
 router.post("/user/:userId/books/read/:bookId", userLibraryController.addToMyReadLibrary);
 
 /**
  * DELETE /user/:userId/books/read/:bookId
  * @summary Remove a book from the "already read" list of a user
- * @param {number} userId.path.required - ID of the user
- * @param {number} bookId.path.required - ID of the book
- * @return {object} 200 - success message
+ * @param {number} userId.path.required - The ID of the user
+ * @param {number} bookId.path.required - The ID of the book
+ * @return {object} 200 - Success message
  */
-router.delete("/user/:userId/books/read/:bookId",userLibraryController.deleteToMyReadLibrary);
+router.delete("/user/:userId/books/read/:bookId", userLibraryController.deleteToMyReadLibrary);
 
 
-//Toread
+// To Read
 
 /**
  * POST /user/:userId/books/to-read/:bookId
  * @summary Add a book to the "to read" list of a user
- * @param {number} userId.path.required - ID of the user
- * @param {number} bookId.path.required - ID of the book
- * @return {object} 200 - success message
+ * @param {number} userId.path.required - The ID of the user
+ * @param {number} bookId.path.required - The ID of the book
+ * @return {object} 200 - Success message
  */
 router.post("/user/:userId/books/to-read/:bookId", userLibraryController.addToWishRead);
 
 /**
  * DELETE /user/:userId/books/to-read/:bookId
  * @summary Remove a book from the "to read" list of a user
- * @param {number} userId.path.required - ID of the user
- * @param {number} bookId.path.required - ID of the book
- * @return {object} 200 - success message
+ * @param {number} userId.path.required - The ID of the user
+ * @param {number} bookId.path.required - The ID of the book
+ * @return {object} 200 - Success message
  */
-router.delete("/user/:userId/books/to-read/:bookId",userLibraryController.deleteToWishRead);
+router.delete("/user/:userId/books/to-read/:bookId", userLibraryController.deleteToWishRead);
+
