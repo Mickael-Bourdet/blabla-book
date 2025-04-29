4a08143b1706f48d4fc75e942f987ecc5d37f9c0
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-04-29 22:18:54 +0200

    feat : first recommended books

--- back/src/database/seed_tables.js
+++ back/src/database/seed_tables.js
@@ -139,6 +139,33 @@ async function seedDatabase() {
         cover_url: "1598474896i/55134619",
         page_count: 393,
       },
+      {
+        isbn: "9782070585205",
+        title: "Harry Potter et la Coupe de Feu",
+        description:
+          "Harry participe au Tournoi des Trois Sorciers, une compétition magique qui met ses capacités à l'épreuve. Ce tome voit son monde s'assombrir, alors qu’il fait face à des épreuves dangereuses et à de nouveaux défis. C'est un tournant majeur dans la saga.",
+        published: 2011,
+        cover_url: "1508475182i/36444262",
+        page_count: 774,
+      },
+      {
+        isbn: "9782290319956",
+        title: "Le Trône de Fer – Tome 5 : L’Invincible Forteresse",
+        description:
+          "Dans un monde où le pouvoir change de mains, ce tome explore les luttes politiques intenses après un grand bouleversement. De nouveaux défis et trahisons surgissent, et les protagonistes se battent pour garder leur place dans un jeu de pouvoir impitoyable.",
+        published: 2002,
+        cover_url: "1330189403i/2072931",
+        page_count: 352,
+      },
+      {
+        isbn: "9791032711262",
+        title: "Frieren : Beyond Journey’s End - Tome 4",
+        description:
+          " Frieren continue son voyage après la fin de la quête des héros. Ce tome se concentre sur la découverte de nouvelles régions et sur les rencontres avec des personnages qui vont remettre en question sa vision du monde et du passage du temps.",
+        published: 2022,
+        cover_url: "1657191679i/61405470",
+        page_count: 188,
+      },
     ];
 
     const books = await Book.bulkCreate(bookData);
---
+++ front/src/data/recommendedBooks.tsx
@@ -0,0 +1,22 @@
+export const recommendedBooks = [
+  {
+    id: 13,
+    isbn: "9782266260787",
+    title: "Hunger Games - Tome 2",
+    description:
+      "Dans un futur dystopique, Katniss Everdeen, une jeune fille, doit participer à un combat à mort télévisé avec d'autres enfants pour divertir les élites. Un récit de survie, de rébellion et de résistance.",
+    published: 2008,
+    cover_url: "1451581575i/28415052",
+    page_count: 432,
+  },
+  {
+    id: 14,
+    isbn: "9782253088752",
+    title: "Frankenstein",
+    description:
+      "Le Dr. Frankenstein créé un monstre à partir de morceaux de cadavres, mais se retrouve horrifié par sa création. Un roman gothique qui interroge la responsabilité scientifique, la solitude et l’éthique.",
+    published: 2009,
+    cover_url: "1364186726i/7261829",
+    page_count: 345,
+  },
+];
