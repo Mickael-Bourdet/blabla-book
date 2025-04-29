94e8985773f2504eaa35e3d73877cf21d111f7ce
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-04-29 22:28:39 +0200

    feat : recommended books

--- front/src/data/recommendedBooks.tsx
+++ front/src/data/recommendedBooks.tsx
@@ -19,4 +19,34 @@ export const recommendedBooks = [
     cover_url: "1364186726i/7261829",
     page_count: 345,
   },
+  {
+    id: 15,
+    isbn: "9782012101401",
+    title: "Astérix chez les Bretons",
+    description:
+      "Astérix et son ami Obélix, deux irréductibles Gaulois, se battent contre les Romains et affrontent divers défis avec humour et esprit. Une série pleine de références à l’histoire et la culture européenne.",
+    published: 2004,
+    cover_url: "1419178691i/900568",
+    page_count: 48,
+  },
+  {
+    id: 16,
+    isbn: "9782212567595",
+    title: "Naruto - Tome 41",
+    description:
+      "Ce tome marque un tournant majeur dans l'histoire, alors que Naruto et ses alliés se lancent dans une guerre contre l'Akatsuki. Des batailles épiques ont lieu, et des révélations importantes sur les origines de certains personnages changent la dynamique de la guerre. L'émotion et l'intensité des combats sont à leur apogée.",
+    published: 2009,
+    cover_url: "1363037606i/6218526",
+    page_count: 192,
+  },
+  {
+    id: 17,
+    isbn: "9782505005582",
+    title: "L’art subtil de s’en foutre",
+    description:
+      "Un livre de développement personnel qui renverse les conventions habituelles. Mark Manson prône une approche plus réaliste du bonheur, en se concentrant sur les choses qui comptent vraiment et en abandonnant la quête d'une vie parfaite",
+    published: 2017,
+    cover_url: "1533568095i/41057918",
+    page_count: 188,
+  },
 ];
