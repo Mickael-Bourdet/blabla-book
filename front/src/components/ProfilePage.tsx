339da02541707486d488edea24d4a3afb71ca141
Axel-Chat <you@example.com>
2025-04-30 17:39:14 +0200

    add: Link route done

--- front/src/components/ProfilePage.tsx
+++ front/src/components/ProfilePage.tsx
@@ -7,9 +7,10 @@ const ProfilePage = () => {
     <div className="p-4 ">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl font-bold">NOM DU PROFIL</h1>
-       <a href="/user/1/settings"><button  className="bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-500">
+        
+        <Link to="/user/1/settings"><button  className="bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-500">
           Modifier le profil
-        </button></a>
+        </button></Link>
       </div>
 
       {/* Livres lus */}
