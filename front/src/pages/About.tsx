790891650b482d0516ffd62e91b03de26e27a94a
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-04-30 20:28:22 +0200

    feat : start design cover book

--- front/src/pages/About.tsx
+++ front/src/pages/About.tsx
@@ -1,17 +1,16 @@
 import HTMLFlipBook from "react-pageflip";
 export default function About() {
   return (
-    <div className="flex justify-center p-20 pb-25 items-stretch  ">
+    <div className="flex justify-center p-20 pb-25 items-stretch">
       <HTMLFlipBook width={500} height={600} size={"fixed"} showCover={true} maxShadowOpacity={0.5} flippingTime={700}>
-        <div className="bg-red-800 text-center items-center rounded--md bg-[linear-gradient(to_right,_rgb(60,_13,_20)_3px,_rgba(255,_255,_255,_0.5)_10px,_rgba(255,_255,_255,_0.25)_7px,_rgba(255,_255,_255,_0.25)_20px,_transparent_24px,_transparent_32px,_rgba(255,_255,_255,_0.25)_34px,_transparent_44px)]">
-          <p>test</p>
-          <p>test</p>
-          <p>test</p>
-          <p>test</p>
-          <p>test</p>
-          <p>test</p>
-          <p>test</p>
-          <p>test</p>
+        <div
+          className="bg-red-800 h-full flex flex-col items-center justify-center 
+    rounded-md bg-[linear-gradient(to_right,_rgb(60,_13,_20)_3px,_rgba(255,_255,_255,_0.5)_10px,_rgba(255,_255,_255,_0.25)_7px,_rgba(255,_255,_255,_0.25)_20px,_transparent_24px,_transparent_32px,_rgba(255,_255,_255,_0.25)_34px,_transparent_44px)]"
+        >
+          <div className="flex flex-col items-center justify-center h-full text-gray-300">
+            <p className=" text-4xl text-center leading-[1.5]">Qui se cache derrière BlaBlaBook</p>
+            <p className="text-center mt-4">Un livre edition O'clock</p>
+          </div>
         </div>
         <div className="bg-white">Page 2</div>
         <div className="bg-white">Page 3</div>
