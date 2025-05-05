bda44bbdca2880e507a803b6f763d53a505a604a
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-05 15:06:37 +0200

    to merge

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <>
-      <footer className="bg-header px-8 py-2 text-base font-bold hidden md:block bottom-0 w-full  ">
+    <>                  
+      <footer className="bg-header px-8 py-2 text-base font-bold hidden md:block bottom-0 w-full pl-64">
         <ul className="flex justify-between items-center">
           <li>
             <Link to="/about">À propos</Link>
@@ -12,7 +12,7 @@ const Footer = () => {
             <Link to="/mentions-legales">Mentions légales</Link>
           </li>
           <li>
-            © Promo Sushi <br /> Équipe BlaBlaBook
+            © Promo Sushi <br/> Équipe BlaBlaBook
           </li>
         </ul>
       </footer>
