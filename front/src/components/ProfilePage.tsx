40e451cc7abc3ba6e0dbfcdbb09c9878e31ff8e8
Bounabassa Kebe <bounabassa.kebe31@gmail.com>
2025-05-05 10:50:13 +0200

    feat des use effets

--- front/src/components/ProfilePage.tsx
+++ front/src/components/ProfilePage.tsx
@@ -1,12 +1,18 @@
-import { popularBooks } from "../data/popularBooks";
-import { recommendedBooks } from "../data/recommendedBooks";
+import { useEffect, useState } from "react";
+import { getOneUser } from "../api/apiBooks";
+import type { IUser } from "../@types";
 import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  
   return (
     <div className="p-4 md:ml-64">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-3xl font-bold">NOM DU PROFIL</h1>
+        <h1 className="text-3xl font-bold">{user.name}</h1>
         <button className="bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-500">
           Modifier le profil
         </button>
@@ -15,21 +21,19 @@ const ProfilePage = () => {
       {/* Livres lus */}
       <section className="mb-10">
         <h2 className="text-2xl font-semibold mb-4">
-          Mes livres lus : {popularBooks.length}
+          Mes livres lus : {user.books_already_read.length}
         </h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
-          {popularBooks.map((book) => (
-            <Link key={book.id} to={`/books/${book.id}`} className="block">
-              <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+          {user.books_already_read.map((book) => (
+            <Link key={book.id} to={`/books/${book.id}`}>
+              <div className="hover:shadow-lg rounded-md transition-shadow">
                 <img
                   src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
                   alt={book.title}
-                  className="h-100 w-full object-cover mb-2"
-                  />
+                  className="w-full h-64 object-cover mb-2 rounded"
+                />
                 <p className="text-center">{book.title}</p>
-
               </div>
-              
             </Link>
           ))}
         </div>
@@ -38,21 +42,19 @@ const ProfilePage = () => {
       {/* Livres à lire */}
       <section>
         <h2 className="text-2xl font-semibold mb-4">
-          Mes livres à lire : {recommendedBooks.length}
+          Mes livres à lire : {user.books_wish_read.length}
         </h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
-          {recommendedBooks.map((book) => (
-            <Link key={book.id} to={`/books/${book.id}`} className="block">
-              <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+          {user.books_wish_read.map((book) => (
+            <Link key={book.id} to={`/books/${book.id}`}>
+              <div className="hover:shadow-lg rounded-md transition-shadow">
                 <img
                   src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
                   alt={book.title}
-                  className="h-100 w-full object-cover mb-2"
+                  className="w-full h-64 object-cover mb-2 rounded"
                 />
                 <p className="text-center">{book.title}</p>
-
               </div>
-
             </Link>
           ))}
         </div>
