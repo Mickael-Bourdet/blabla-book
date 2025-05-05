c61a17be400f64299b7d367f32c0b071a8ac1d55
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-05 15:07:16 +0200

    to merge

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>                  
-      <footer className="bg-header px-8 py-2 text-base font-bold hidden md:block bottom-0 w-full pl-64">
+      <footer className="bg-header px-8 py-2 text-base font-bold hidden md:block bottom-0 w-full ">
         <ul className="flex justify-between items-center">
           <li>
             <Link to="/about">À propos</Link>
