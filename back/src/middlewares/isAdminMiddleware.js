87ee67c166535c6f2a9931aa67924dcec73971dc
AmndineBlt <amandine.bluteau@oclock.school>
2025-05-06 15:24:31 +0200

    add: isAdminMiddleware

---
+++ back/src/middlewares/isAdminMiddleware.js
@@ -0,0 +1,22 @@
+import { User } from "../models/User.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const isAdminMiddleware = async (req, _res, next) => {
+  try {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+      return next(new ApiError("Non authentifié", 401));
+    }
+
+    const user = await User.findByPk(userId);
+
+    if (!user || user.role !== "admin") {
+      return next(new ApiError("Accès refusé : admin requis", 403));
+    }
+
+    next(); // accès autorisé
+  } catch (err) {
+    next(err);
+  }
+};
