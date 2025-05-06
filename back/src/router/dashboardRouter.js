0ac26d87931f3acedbb560b78c1e874e30075a42
AmndineBlt <amandine.bluteau@oclock.school>
2025-05-06 18:42:15 +0200

    update: add middleware auth and isAdmin

--- back/src/router/dashboardRouter.js
+++ back/src/router/dashboardRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import dashboardController from "../controller/dashboardController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { isAdminMiddleware } from "../middlewares/isAdminMiddleware";
 
 export const router = Router();
 
-router.get("/dashboard/categories", dashboardController.getAllCategories);
-router.get("/dashboard/users", dashboardController.getAllUsers);
-router.get("/dashboard/authors", dashboardController.getAllAuthors);
+router.get("/dashboard/categories", authMiddleware, isAdminMiddleware, dashboardController.getAllCategories);
+router.get("/dashboard/users", authMiddleware, isAdminMiddleware, dashboardController.getAllUsers);
+router.get("/dashboard/authors", authMiddleware, isAdminMiddleware, dashboardController.getAllAuthors);
