53472835eea88a20f52d004a26a6aa70884a6bb4
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-06 14:02:41 +0200

    feat : change getAllBooks function in apiBooks to get url params

--- front/src/api/apiBooks.ts
+++ front/src/api/apiBooks.ts
@@ -1,9 +1,28 @@
 import type { IBooks, IBook } from "../@types";
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-console.log(import.meta.env.VITE_API_BASE_URL);
 
-export async function getAllBooks(): Promise<IBooks> {
-  const response = await fetch(`${apiBaseUrl}/books`);
+export async function getAllBooks(params = {}): Promise<IBooks> {
+  const urlParams = new URLSearchParams();
+
+  // transform params as an object like [key, value]
+  const paramsObject = Object.entries(params);
+
+  paramsObject.forEach(([key, value]) => {
+    // check if the value is undefined or null
+    if (value !== undefined && value !== null) {
+      // convert value into string and add it into URL params
+      urlParams.append(key, value.toString());
+    }
+  });
+
+  // convert urlParams object into a string
+  const urlString = urlParams.toString();
+
+  // add params in the API request if exists
+  const newUrl = `${apiBaseUrl}/books/${urlString ? `?${urlString}` : ""}`;
+
+  // request to the new URL
+  const response = await fetch(newUrl);
   const books = await response.json();
   return books;
 }
