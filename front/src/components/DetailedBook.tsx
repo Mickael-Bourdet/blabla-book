000e5a3499f25d5e03f13c5a2c79d6dc09b60303
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-06 14:25:34 +0200

    fix: responsive detailedBook

--- front/src/components/DetailedBook.tsx
+++ front/src/components/DetailedBook.tsx
@@ -73,71 +73,68 @@ const BookDetail = () => {
   }
 
   return (
-    <div className="md:ml-64">
-      {/* Ajoute une marge à gauche sur les écrans md et plus grands */}
-      <section className="bg-beige-50 p-4 md:p-8">
-        <div className="flex flex-row items-center md:flex-row  md:gap-8 mt-15">
-          <img
-            src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
-            alt={`${book.title}`}
-            className="w-30 mr-5 h-auto mb-4 md:w-60"
-          />
+    <section className="bg-beige-50 p-4 flex items-center md:p-8 md:ml-50">
+      <div className="flex flex-row items-center md:flex-row  md:gap-8 mt-5">
+        <img
+          src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
+          alt={`${book.title}`}
+          className="w-30 mr-5 h-auto mb-4 md:w-60"
+        />
 
-          <div className="text-sm md:text-base max-w-xl">
-            <p>
-              <span className="font-semibold">Par</span> :{" "}
-              {book.authors.map((auth) => auth.name).join(", ")}
-            </p>
+        <div className="text-sm md:text-base max-w-xl">
+          <p>
+            <span className="font-semibold">Par</span> :{" "}
+            {book.authors.map((auth) => auth.name).join(", ")}
+          </p>
 
-            <h1 className="text-lg font-bold mb-2">{book.title}</h1>
+          <h1 className="text-lg font-bold mb-2">{book.title}</h1>
 
-            <p>
-              <span className="font-semibold">Catégorie</span> :{" "}
-              {book.categories.map((cat) => cat.name).join(", ")}
-            </p>
+          <p>
+            <span className="font-semibold">Catégorie</span> :{" "}
+            {book.categories.map((cat) => cat.name).join(", ")}
+          </p>
 
-            <p className="mb-2">
-              <span className="font-semibold">Date de publication</span> :{" "}
-              {book.published}
-            </p>
+          <p className="mb-2">
+            <span className="font-semibold">Date de publication</span> :{" "}
+            {book.published}
+          </p>
 
-            <p className="font-semibold mt-4 mb-1">Description :</p>
-            <p>{book.description}</p>
+          <p className="font-semibold mt-4 mb-1">Description :</p>
+          <p>{book.description}</p>
 
-            <div className="flex gap-20 mt-4 ml-30">
-              <button
-                onClick={!isRead ? handleAddRead : handleRemoveRead}
-                className={`flex items-center gap-2 ${
+          <div className="flex flex-col items-center gap-4 lg:flex-row mt-4">
+            <button
+              onClick={!isRead ? handleAddRead : handleRemoveRead}
+              className={`flex items-center gap-2 ${
+                isRead && !toRead
+                  ? `bg-green-300 hover:bg-green-200 ${!toRead}`
+                  : "bg-gray-300 hover:bg-gray-200"
+              }  rounded px-10 py-2 cursor-pointer`}
+            >
+              <i
+                className={`${
                   isRead && !toRead
-                    ? `bg-green-300 hover:bg-green-200 ${!toRead}`
-                    : "bg-gray-300 hover:bg-gray-200"
-                }  rounded px-10 py-2 cursor-pointer`}
-              >
-                <i
-                  className={`${
-                    isRead && !toRead
-                      ? "fa-solid fa-square-check"
-                      : "fa-solid fa-eye"
-                  }`}
-                ></i>
-                <span>{`${isRead && !toRead ? "Lu" : "Non Lu"}`}</span>
-              </button>
-              <button
-                onClick={!toRead ? handleWishRead : handleRemoveWishRead}
-                className={`flex items-center gap-2 ${
-                  toRead && !isRead
-                    ? "bg-green-300 hover:bg-green-200"
-                    : "bg-gray-300 hover:bg-gray-200"
-                } rounded px-10 py-2 cursor-pointer`}
-              >
-                <i className="fa-solid fa-book-open-reader"></i>
-                <span>À Lire</span>
-              </button>
-            </div>
+                    ? "fa-solid fa-square-check"
+                    : "fa-solid fa-eye"
+                }`}
+              ></i>
+              <span>{`${isRead && !toRead ? "Lu" : "Non Lu"}`}</span>
+            </button>
+            <button
+              onClick={!toRead ? handleWishRead : handleRemoveWishRead}
+              className={`flex items-center gap-2 ${
+                toRead && !isRead
+                  ? "bg-green-300 hover:bg-green-200"
+                  : "bg-gray-300 hover:bg-gray-200"
+              } rounded px-10 py-2 cursor-pointer`}
+            >
+              <i className="fa-solid fa-book-open-reader"></i>
+              <span>À Lire</span>
+            </button>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
