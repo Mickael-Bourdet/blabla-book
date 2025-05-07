f0b8e2cfd36594b6645e736586a65dc19ad02ebc
AmndineBlt <amandine.bluteau@oclock.school>
2025-05-07 14:48:27 +0200

    update: remove comments

--- front/src/utils/store/useAuthStore.ts
+++ front/src/utils/store/useAuthStore.ts
@@ -19,15 +19,13 @@ export const useAuthStore = create<AuthState>()(
       user: null,
       token: null,
 
-      // Fonction appelée quand on se connecte
       login: (name, id, token) => set(() => ({user : {name, id}, token})),
 
-      // Fonction appelée quand on se déconnecte
       logout: () => set({ user: null, token: null }),
     }),
     {
-      name: "auth-storage", // clé utilisée dans localStorage
-      partialize: (state) => ({ user: state.user, token: state.token }), // on stocke seulement ce qu'on veut (pas les fonctions)
+      name: "auth-storage",
+      partialize: (state) => ({ user: state.user, token: state.token }),
     }
   )
 );
