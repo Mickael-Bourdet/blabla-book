72c62bf3f73c5fda8af7816fffed7502de21c3e0
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-09 18:55:31 +0200

    feat: reduce padding bottom in authentication

--- front/src/pages/Authentication.tsx
+++ front/src/pages/Authentication.tsx
@@ -45,7 +45,7 @@ const Authentication = () => {
   };
 
   return (
-    <section className="pb-18 ">
+    <section className="pb-6">
       <Login />
       <Register data={registerData} onChange={setRegisterDate} onSubmit={handleRegister} />
     </section>
