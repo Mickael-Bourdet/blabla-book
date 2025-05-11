8e3b3e49a1471591e4eb8d81e531142092ff1199
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-11 19:09:13 +0200

    refactor : remove mobile nav in header

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -43,29 +43,6 @@ const Header = () => {
           <i className="fa-solid fa-sliders text-2xl text-placeholder"></i>
         </Link>
       </header>
-
-      {/* Navigation mobile */}
-      <div className="fixed bottom-0 left-0 w-full bg-body border-t-1 border-placeholder flex justify-around p-4 z-10 rounded-t md:hidden font-title ">
-        <Link to="/" className="hover:text-blue-600 flex flex-col items-center text-sm">
-          <i className="fa-solid fa-house-chimney"></i> Accueil
-        </Link>
-        <Link to="/library" className="hover:text-blue-600 flex flex-col items-center text-sm">
-          <i className="fa-solid fa-book"></i> Bibliothèque
-        </Link>
-
-        {/* Application de la même condition pour la version mobile */}
-        {!user ? (
-          <Link to="/auth" className="hover:text-blue-600 flex flex-col items-center text-sm">
-            <i className="fa-solid fa-user"></i>
-            Mon compte
-          </Link>
-        ) : (
-          <Link to="/profile" className="hover:text-blue-600 flex flex-col items-center text-sm">
-            <i className="fa-solid fa-user"></i>
-            {user.name}
-          </Link>
-        )}
-      </div>
     </div>
   );
 };
