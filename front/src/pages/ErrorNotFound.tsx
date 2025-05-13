9157b09c0adc919e38fe30850b33bc11cf3cc307
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-13 14:12:45 +0200

    fix : error pages img path

--- front/src/pages/ErrorNotFound.tsx
+++ front/src/pages/ErrorNotFound.tsx
@@ -4,7 +4,7 @@ export default function ErrorNotFound() {
   return (
     <div className="bg-body flex flex-col items-center justify-center pb-4 md:pb-0">
       <h1 className="font-title text-9xl text-center font-black p-7">404</h1>
-      <img className="w-120 mt-4 mx-auto" src="/error404_homme.webp" alt="" />
+      <img className="w-120 mt-4 mx-auto" src="/img/errorPage/error404_homme.webp" alt="" />
       <p className="font-title text-6xl text-center font-bold pt-6">Oups, page introuvable !</p>
       <p className="font-body text-2xl text-center p-6">
         On dirait que cette page s'est perdue entre deux chapitres...
