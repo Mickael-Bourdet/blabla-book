5773b0a452684005b045da7202f8f422f329f3dd
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-13 10:57:54 +0200

    docs: deleteToMyReadLibrary in apiUsers

--- front/src/api/apiUser.ts
+++ front/src/api/apiUser.ts
@@ -1,7 +1,7 @@
 import type { IBook, IUser, IUserUpdate } from "../@types";
 import { authFetch } from "../utils/authFetch";
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;;
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export async function getOneUser(): Promise<IUser | null> {
   const response = await authFetch(`${apiBaseUrl}/user`);
@@ -45,9 +45,26 @@ export async function addToMyReadLibrary(bookId: number): Promise<IBook> {
   return book;
 }
 
+/**
+ * @function deleteToMyReadLibrary
+ * @description Deletes a book from the user's "read" library.
+ *
+ * @param {number} bookId - The ID of the book to delete.
+ * @returns {Promise<boolean>} A promise that resolves to true if the deletion was successful, false otherwise.
+ *
+ * @throws Will throw an error if the request fails (e.g., network error or non-OK response).
+ */
 export async function deleteToMyReadLibrary(bookId: number) {
-  const response = await authFetch(`${apiBaseUrl}/user/books/read/${bookId}`, { method: "DELETE" });
-  return response.ok;
+  try {
+    const response = await authFetch(`${apiBaseUrl}/user/books/read/${bookId}`, { method: "DELETE" });
+    if (!response.ok) {
+      throw new Error(`Impossible de supprimer ce livre`)
+    }
+
+    return response.ok;
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export async function addToWishRead(bookId: number): Promise<IBook> {
