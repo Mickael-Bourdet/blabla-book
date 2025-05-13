5e6596c0c4420557ef1df4641250bb1c7f70bd71
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-13 11:14:56 +0200

    doc: addToWishRead in apiUser

--- front/src/api/apiUser.ts
+++ front/src/api/apiUser.ts
@@ -54,25 +54,46 @@ export async function addToMyReadLibrary(bookId: number): Promise<IBook> {
  *
  * @throws Will throw an error if the request fails (e.g., network error or non-OK response).
  */
-export async function deleteToMyReadLibrary(bookId: number) {
+export async function deleteToMyReadLibrary(bookId: number): Promise<boolean> {
   try {
     const response = await authFetch(`${apiBaseUrl}/user/books/read/${bookId}`, { method: "DELETE" });
     if (!response.ok) {
-      throw new Error(`Impossible de supprimer ce livre`)
+      throw new Error(`Impossible de supprimer ce livre : ${response.statusText}`)
     }
+    return true;
 
-    return response.ok;
   } catch (error) {
-    console.error(error)
+    console.error(error);
+    return false;
   }
 }
 
+/**
+ * @function addToWishRead
+ * @description Adds a book to the user's "to-read" (wishlist) library.
+ *
+ * @param {number} bookId - The ID of the book to add to the wishlist.
+ * @returns {Promise<IBook>} A promise that resolves to the added book object.
+ *
+ * @throws Will throw an error if the request fails or the response is not OK.
+ */
 export async function addToWishRead(bookId: number): Promise<IBook> {
-  const response = await authFetch(`${apiBaseUrl}/user/books/to-read/${bookId}`, {
-    method: "POST",
-  });
-  const book = await response.json();
-  return book;
+  try {
+    const response = await authFetch(`${apiBaseUrl}/user/books/to-read/${bookId}`, {
+      method: "POST",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Impossible d'ajouter ce livre à la liste "à lire" : ${response.statusText}`)
+    }
+
+    const book = await response.json();
+    return book;
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export async function deleteToWishRead(bookId: number) {
