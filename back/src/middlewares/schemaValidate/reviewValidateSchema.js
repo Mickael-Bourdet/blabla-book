657dc1e21a297a90cf2282ea4d2af0674da072db
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-14 11:19:11 +0200

    delete: all reviews stuff

--- back/src/middlewares/schemaValidate/reviewValidateSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const reviewSchema = Joi.object({
-  rating: Joi.number().integer().min(0).max(5),
-  title: Joi.string().max(100),
-  comment: Joi.string().max(1000),
-});
-
-export { reviewSchema };
